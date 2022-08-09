cd469e1972fef300dce3c385014e7c146c76d9fe
uid100 <daniel.f.sullvian@gmail.com>
2022-08-08 18:01:28 -0700

    added Help links, sample.json template, published and updated README

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
+![icon](images/canvas-logo-1024x1020.png)
+[url](https://canvascourseplanner.azurewebsites.net)
+
 # Canvas Course Planning utility
 
-![icon](![icon](images/canvas-logo-1024x1020.png)
 MVC (C#) utility to plan and grapically view Canvas course material.
 
 The application uses dependency injection to support data IO from moq, json, API, or database interface. 
@@ -8,3 +10,6 @@ The application uses dependency injection to support data IO from moq, json, API
 TODO:
  - fix JSON null reference reads from JSON repository model
  - add EF support for SQL (and/or SQLite)
+
+
+![https://canvascourseplanner.azurewebsites.net](https://canvascourseplanner.azurewebsites.net)
---
+++ course/Controllers/HelpController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.DependencyResolver;
+using System.IO;
+using Microsoft.Extensions.Hosting;
+
+namespace course.Controllers
+{
+    public class HelpController : Controller
+    {
+        private IWebHostEnvironment env;
+        public HelpController(IWebHostEnvironment _env)
+        {
+            env = _env;
+        }
+
+        public IActionResult HelpIndex() => View();
+
+        public FileResult Sample()
+        {
+            string fileName = "sample.json";
+            string filePath = Path.Combine(this.env.WebRootPath, fileName);
+            byte[] bytes = System.IO.File.ReadAllBytes(filePath);
+            return File(bytes, "text/plain", fileName);
+        }
+    }
+}
