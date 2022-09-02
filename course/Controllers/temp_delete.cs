3d49482e32221a071d16615a26f0023264233135
uid100 <daniel.f.sullvian@gmail.com>
2022-09-01 19:15:42 -0700

    COP2551 module 3 COP4813 module 3 & 4 course.json updates

---
+++ course/Controllers/temp_delete.cs
@@ -0,0 +1,12 @@
+﻿namespace course.Controllers
+{
+    public class temp_delete
+    {
+        void deleteMe()
+        {
+            Random r = new Random(6543);
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(r.Next());
+        }
+    }
+}
